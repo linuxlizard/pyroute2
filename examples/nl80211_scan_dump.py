438e8b052fb314d9c5ff6dfbd59b4d8f051f9023
David Poole <dpoole@cradlepoint.com>
2019-07-07 17:24:08 -0600

    more IE decodes; use LUT for IE prints
    - add prints for RSN, VHT Operation
    - switch to a LUT to connect IE-ID to a print function

--- examples/nl80211_scan_dump.py
+++ examples/nl80211_scan_dump.py
@@ -20,6 +20,26 @@ logger.setLevel(level=logging.DEBUG)
 
 logging.getLogger("pyroute2").setLevel(level=logging.DEBUG)
 
+def print_ssid(ssid):
+    # Be VERY careful with the SSID!  Can contain hostile input.
+    # For example, this print is vulnerable to an SSID with terminal escape
+    # chars. https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    if len(ssid.data):
+        ssid_hex = hexdump(ssid.data)
+        print("\tSSID: {}\n\tSSID hex: {}".format(
+              ssid.pretty_print(), ssid_hex))
+    else:
+        # empty/null SSID
+        print("\tSSID:")
+
+def print_supported_rates(supp_rates):
+    print("\tSupported rates: {}".format(supp_rates.pretty_print()))
+
+def print_extended_supported_rates(ext_supp_rates):
+    print("\Extended Upported rates: {}".format(ext_supp_rates.pretty_print()))
+
+def print_channel(channel):
+    print("\tDS Parameter set: {}".format(channel.pretty_print()))
 
 def print_ext_capabilities(extcapa):
     if not extcapa:
@@ -49,7 +69,7 @@ def print_ht_capabilities(ht_capa):
 
     print("\tHT capabilities:")
     # TODO get actual value
-    print("\t\tcapabilities: %#06x" % 0)
+    print("\t\tcapabilities: -1")
 
     # Capability Information field
     _, ht_cap_info = ht_capa[0]
@@ -118,7 +138,7 @@ def print_vht_capabilities(vht_capa):
         return
 
     # TODO need a value for the 32-bit cap info field
-    print("\tVHT capabilities:\n\t\tVHT Capabilities ({0:#010x}):".format(0))
+    print("\tVHT capabilities:\n\t\tVHT Capabilities (-1):")
 
     _, vht_cap_info = vht_capa[0]
     for i, field in enumerate(vht_cap_info):
@@ -137,6 +157,47 @@ def print_vht_capabilities(vht_capa):
         if s:
             print("\t\t\t%s" % s)
 
+def print_vht_operation(vht_oper):
+    if not vht_oper:
+        return
+
+    print("\tVHT operation:")
+    oper_info = vht_oper.fields[0]
+    chwidth = oper_info.value[0].value
+    print("\t\t* channel width: {} ({})".format(chwidth, vht_oper.channel_width_str(chwidth)))
+    print("\t\t* center freq segment 1: {}".format(oper_info.value[1].value))
+    print("\t\t* center freq segment 2: {}".format(oper_info.value[2].value))
+    print("\t\t* VHT basic MCS set: {0:#06x}".format(vht_oper.fields[1].value))
+
+def print_rsn(rsn):
+    # 9.4.2.25 RSNE 80211_2016.pdf
+    print("\tRSN:\t* Version: {}".format(rsn.fields[0].value))
+    group_cipher = rsn.fields[1].value
+    cipher_name, = [ v for v in group_cipher if v.name == "Suite Name"]
+    print("\t\t* Group cipher: {}".format(cipher_name.value))
+
+    pair_cipher = rsn.fields[3].value
+    cipher_names = [v.value for pair in pair_cipher for v in pair if v.name == "Suite Name"]
+    print("\t\t* Pairwise ciphers: {}".format(" ".join(cipher_names)))
+
+    akm_suite = rsn.fields[5].value
+    cipher_names = [v.value for akm in akm_suite for v in akm if v.name == "Suite Name"]
+    print("\t\t* Authentication suites: {}".format(" ".join(cipher_names)))
+
+    # slide the PTKSA and GTKSA fields in between to match how the 
+    # iw scan.c _print_rsn_ie() does decode
+    def capa_str(field):
+        # a few special cases
+        if field.name == "PTKSA-RC" or field.name == "GTKSA-RC":
+            return "{}-{}".format(2**field.value, field.name)
+        if field.value:
+            return field.name
+        return None
+
+    rsn_capa = rsn.fields[6]
+    capa_list = [capa_str(v) for v in rsn_capa.value if v is not None]
+    capa_list = [v for v in capa_list if v is not None]
+    print("\t\t* Capabilities: {} ({})".format(" ".join(capa_list), -1))
 
 def print_bss(bss):
     # NOTE: the contents of beacon and probe response frames may or may not
@@ -156,59 +217,26 @@ def print_bss(bss):
     # each IE should be an instance of nl80211.IE
     ies = bss['NL80211_BSS_INFORMATION_ELEMENTS']
 
-    # Be VERY careful with the SSID!  Can contain hostile input.
-    # For example, this print is vulnerable to an SSID with terminal escape
-    # chars. https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-    if len(ies["NL80211_BSS_ELEMENTS_SSID"].data):
-        ssid_hex = hexdump(ies["NL80211_BSS_ELEMENTS_SSID"].data)
-        print("\tSSID: {}\n\tSSID hex: {}".format(
-              ies["NL80211_BSS_ELEMENTS_SSID"].pretty_print(), ssid_hex))
-    else:
-        # empty/null SSID
-        print("\tSSID:")
+    ie_printers = (
+        ("NL80211_BSS_ELEMENTS_SSID", print_ssid),
+        ("NL80211_BSS_ELEMENTS_SUPPORTED_RATES", print_supported_rates),
+        ("NL80211_BSS_ELEMENTS_CHANNEL", print_channel),
+        ("NL80211_BSS_ELEMENTS_COUNTRY", print_country),
+        ("NL80211_BSS_ELEMENTS_EXTENDED_RATES", print_extended_supported_rates),
+        ("NL80211_BSS_ELEMENTS_HT_CAPABILITIES", print_ht_capabilities),
+        ("NL80211_BSS_ELEMENTS_HT_OPERATION", print_ht_operation),
+        ("NL80211_BSS_ELEMENTS_EXT_CAPABILITIES", print_ext_capabilities),
+        ("NL80211_BSS_ELEMENTS_VHT_CAPABILITIES", print_vht_capabilities),
+        ("NL80211_BSS_ELEMENTS_VHT_OPERATION", print_vht_operation),
+        ("NL80211_BSS_ELEMENTS_RSN", print_rsn),
+        # TODO more IE decodes
+    )
 
-    # Supported Rates.
-    print("\tSupported rates: {}".format(
-          ies["NL80211_BSS_ELEMENTS_SUPPORTED_RATES"].pretty_print()))
-
-    try:
-        print("\tDS Parameter set: {}".format(
-              ies["NL80211_BSS_ELEMENTS_CHANNEL"].pretty_print()))
-    except KeyError:
-        pass
-
-    try:
-        print_country(ies["NL80211_BSS_ELEMENTS_COUNTRY"])
-    except KeyError:
-        pass
-
-    try:
-        print("\tExtended supported rates: {}".format(
-              ies["NL80211_BSS_ELEMENTS_EXTENDED_RATE"].pretty_print()))
-    except KeyError:
-        pass
-
-    try:
-        print_ht_capabilities(ies["NL80211_BSS_ELEMENTS_HT_CAPABILITIES"])
-    except KeyError:
-        pass
-
-    try:
-        print_ht_operation(ies["NL80211_BSS_ELEMENTS_HT_OPERATION"])
-    except KeyError:
-        pass
-
-    try:
-        print_ext_capabilities(ies["NL80211_BSS_ELEMENTS_EXT_CAPABILITIES"])
-    except KeyError:
-        pass
-
-    try:
-        print_vht_capabilities(ies["NL80211_BSS_ELEMENTS_VHT_CAPABILITIES"])
-    except KeyError:
-        pass
-
-    # TODO more IE decodes
+    for p in ie_printers:
+        try:
+            p[1](ies[p[0]])
+        except KeyError:
+            pass
 
 
 def main(ifname):
